90405943d1773a4c9c76fc977f69b65ab4dacf53
SMXaS <jurkisarnis@gmail.com>
2021-02-27 02:55:55 +0000

    finish crud func

---
+++ README.md
@@ -0,0 +1,21 @@
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+[![MIT License][license-badge]](LICENSE.md)
+# RESTFUL-DJANGO-PYTHON-ROOM-BOOKING-SYSTEM
+ * RESTFUL DJANGO BACKEND API FOR ROOM BOOKING SYSTEM
+
+## Usage
+* GET BOOKING
+* CREATE BOOKING
+* UPDATE BOOKING
+* DELETE BOOKING
+
+## Technology
+ * Python 3.9.1 64-bit
+ * Django RESTFUL Framework
+ * Django cors headers
+
+ ## Tested with Postman
+ 
+## Contributing
+* Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
--- booking_app/views.py
+++ booking_app/views.py
@@ -1,14 +1,49 @@
 from rest_framework import generics
 
-
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from . import models
+from . import serializers
 from .models import Room
 from .serializers import BookingsSerializer
 
 
-class ListAllBookings(generics.ListAPIView):
-    queryset = Room.objects.all()
-    serializer_class = BookingsSerializer
-
-class DetailBooking(generics.RetrieveAPIView):
-    queryset = Room.objects.all()
-    serializer_class = BookingsSerializer
+class ListAllBookings(APIView):
+    
+    def get(self, request):
+        articles = models.Room.objects.all()
+        serializer = serializers.BookingsSerializer(articles,   
+                                                  many=True)
+        return Response(serializer.data)
+    
+    def post(self, request):
+        serializer=serializers.BookingsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data,
+                           status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, 
+                       status=status.HTTP_400_BAD_REQUEST)
+class DetailBooking(APIView):
+    
+    def get(self, request, pk):
+        article = models.Room.objects.get(id=pk)
+        serializer = serializers.BookingsSerializer(article)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        article = models.Room.objects.get(id=pk)
+        serializer = serializers.BookingsSerializer(article, 
+                                                  request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, 
+                       status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        article = models.Room.objects.get(id=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
